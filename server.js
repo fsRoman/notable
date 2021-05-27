9cb283d721f611fab433075f58fa0e5c57f2252b
fsRoman <ebengard94@gmail.com>
2021-05-27 17:59:15 +0300

    feat: create base routes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
 const app = express();
 
 const port = 8000;
+
+app.use(express.urlencoded());
+
+require('./app/routes') (app, {});
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
