613dc61782c394c1569a919d4c7eef32f27442ec
fsRoman <ebengard94@gmail.com>
2021-05-28 09:48:25 +0300

    feat: implement connection to db

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const db = require('./config/db');
 const app = express();
 
 const port = 8000;
 
 app.use(express.urlencoded());
 
-require('./app/routes') (app, {});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  
+  require('./app/routes') (app, database);
 
-app.listen(port, () => {
-  console.log(`Server started on port: ${port}`);
+  app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+  });
 });
