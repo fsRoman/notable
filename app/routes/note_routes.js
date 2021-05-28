e7e4a746023157421a49707ff43fa25e0101b26c
fsRoman <ebengard94@gmail.com>
2021-05-28 15:52:09 +0300

    feat: implement insert and get items to db

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,33 @@
+const ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    
+    db.collection('notes')
+      .findOne(details, (e, item) => {
+        if (e) {
+          console.log(e);
+          res.send(e);
+        } else {
+          res.send(item);
+        };
+      });
+  })
   app.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.send('Hello')
+    const note = {
+      text: req.body.body,
+      title: req.body.title
+    };
+
+    db.collection('notes')
+      .insertOne(note, (e, result) => {
+        if (e) {
+          console.log(e);
+          res.send(e);
+        } else {
+          res.send(result.ops[0]);
+        };
+      });
   });
 };
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const db = require('./config/db');
+const dbConfig = require('./config/db');
 const app = express();
 
 const port = 8000;
 
 app.use(express.urlencoded());
 
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
+MongoClient.connect(dbConfig.url).then(connection => {
   
-  require('./app/routes') (app, database);
+  const db = connection.db(connection.dbName);
+  require('./app/routes') (app, db);
 
   app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
   });
+}).catch(err => {
+  console.log("ERROR connection ", err)
 });
