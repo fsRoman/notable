2c4c40692cc1bbdbd1c9b1d1fd1a989fb8aa41d8
fsRoman <ebengard94@gmail.com>
2021-05-28 16:06:11 +0300

    feat: implement delete item from db

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -13,7 +13,23 @@ module.exports = function(app, db) {
           res.send(item);
         };
       });
-  })
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    
+    db.collection('notes')
+      .deleteOne(details, (e, item) => {
+        if (e) {
+          console.log(e);
+          res.send(e);
+        } else {
+          res.send(item);
+        };
+      });
+  });
+
   app.post('/notes', (req, res) => {
     const note = {
       text: req.body.body,
